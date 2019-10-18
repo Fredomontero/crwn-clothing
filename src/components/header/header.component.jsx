e254eed71262b2d5652ae2e623c73e0cdaf32db2
Christian <christianmontero08@gmail.com>
2019-10-18 16:36:58 -0700

    Implement user reducers and user actions to header and App components

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { connect } from 'react-redux'
+
 import './header.styles.scss'
 import { ReactComponent as Logo } from '../../assets/crown.svg'
 import { auth } from '../../firebase/firebase.utils'
@@ -20,8 +22,11 @@ const Header = ({currentUser}) => (
                 <Link to='/signin' className='option'>SIGN IN</Link>
             }
         </div>
-
     </div>
-)
+);
 
-export default Header;
+const mapStateToProps = state => ({
+    currentUser: state.user.currentUser
+})
+
+export default connect(mapStateToProps)(Header);
---
+++ src/redux/store.js
@@ -0,0 +1,11 @@
+import{ createStore, applyMiddleware } from 'redux'
+
+//so if we remember we need add middleware to our store, so when whatever actions gets fired or dispatched, we can catch them and display them
+
+import logger from 'redux-logger';
+import rootReducer from './root-reducer';
+
+const middlewares = [logger];
+const store = createStore(rootReducer, applyMiddleware(...middlewares))
+
+export default store;
