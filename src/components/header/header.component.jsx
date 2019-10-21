206eb936d74c7ed55a9a04c9989e879796643e62
Christian Montero Martínez <christianmontero08@gmail.com>
2019-10-20 23:53:39 -0700

    Add cart-dropdown component

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { connect } from 'react-redux'
+import  CartIcon from '../cart-icon/cart-icon.component'
+import CartDropdown from '../cart-dropdown/cart-dropdown.component'
 
 import './header.styles.scss'
 import { ReactComponent as Logo } from '../../assets/crown.svg'
@@ -21,7 +23,9 @@ const Header = ({currentUser}) => (
                 <div className='option' onClick={ () => auth.signOut() }>SIGN OUT</div>) : (
                 <Link className='option' to='/signin'>SIGN IN</Link>
                 )}
+            <CartIcon/>
         </div>
+        <CartDropdown/>
     </div>
 );
 
