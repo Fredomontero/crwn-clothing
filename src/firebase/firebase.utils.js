ba91eb60ca210f7a5c416169b4f1d98f24e4a8bf
Christian <christianmontero08@gmail.com>
2019-10-23 19:45:25 -0700

    Upload items to firestore

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -1,6 +1,8 @@
 import firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/auth'
+import rootReducer from '../redux/root-reducer';
+import { identifier } from '@babel/types';
 
 const config = {
     apiKey: "AIzaSyARu6mGjR4QOUADR7LwcAU6t8w-xQ-Fbao",
@@ -37,6 +39,39 @@ export const createUserProfileDocument = async (userAuth, additionalData) => {
 }
 
 firebase.initializeApp(config);
+
+export const addCollectionsAndDocuments = async (collectionKey, objectsToAdd) => {
+    const collectionRef = firestore.collection(collectionKey);
+    console.log(collectionRef);
+
+    const batch = firestore.batch();
+    objectsToAdd.forEach(obj => {
+        const newDocRef = collectionRef.doc();
+        batch.set(newDocRef, obj);
+        console.log(newDocRef);
+    });
+
+    return await batch.commit();
+
+}
+
+export const convertCollectionsSnapshotToMap = (collections) => {
+    const transformedCollection = collections.docs.map(doc => {
+        const {title, items} = doc.data();
+
+        return{
+            routeName: encodeURI(title.toLowerCase()),
+            id: doc.identifier,
+            title,
+            items
+        };
+    });
+    
+    return transformedCollection.reduce((accumulator, collection) => {
+        accumulator[collection.title.toLowerCase()] = collection;
+        return accumulator;
+    }, {});
+}
 
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
